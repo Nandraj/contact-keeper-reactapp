058db29c51781cfae4d8052251458fcc08f8a60e
Nandraj <nandraj.rathod.009@gmail.com>
2021-07-22 12:21:17 +0530

    registration func which returns JWT complete

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connnect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to contact keeper API...' })
 );
