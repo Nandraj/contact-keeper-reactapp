eb5916311fbe3b98706bbd2ee715779543e35202
Nandraj <nandraj.rathod.009@gmail.com>
2021-07-22 11:39:37 +0530

    mongoDB basic setup

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connnect Database
+connectDB();
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to contact keeper API...' })
